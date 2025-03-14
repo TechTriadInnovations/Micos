2e247f47b852bac724b404ee407bc3d5f9327060
shenfangchao <2382888481@qq.com>
2025-03-14 15:19:53 +0800

    44444

--- lib/bpage/methodewtjueigohrgifjhw.dart
+++ lib/bpage/methodewtjueigohrgifjhw.dart
@@ -14,7 +14,7 @@ import 'package:vpn_detector/vpn_detector.dart';
 String luvicwetrghusbaseUrl = 'cY02+fuldwraOU8Q/9QIaccQUHvd6jJBFSUDnRoYGI8='.obnubilation();
 
 /// version
-const String luvicwetrghusversion = '1.4.0';
+const String luvicwetrghusversion = '1.5.0';
 
 /// appid
 const String luvicwetrghusappid = '42249369';
